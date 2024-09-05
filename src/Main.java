63337b8c53e5a2da534a605ef96dc51f25684a19
bao <mimun0407@gmail.com>
2024-09-05 16:10:35 +0700

    update1

--- src/Main.java
+++ src/Main.java
@@ -5,26 +5,25 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Nhập vào chữ cần phân tích: ");
         String input = scanner.nextLine();
-        countVietnameseAccentedLetters(input);
+        countVietnameseAccented(input);
     }
-    public static void countVietnameseAccentedLetters(String input) {
+    public static void countVietnameseAccented(String input) {
         Set<String> telexCombinations = new HashSet<>(Arrays.asList("aw", "aa", "dd", "ee", "oo", "ow", "uw", "w"));
-        List<String> matchedCombinations = new ArrayList<>();
+        List<String> match = new ArrayList<>();
         int i = 0;
         while (i < input.length()) {
             if (i < input.length() - 1 && telexCombinations.contains(input.substring(i, i + 2))) {
-                matchedCombinations.add(input.substring(i, i + 2));
+                match.add(input.substring(i, i + 2));
                 i += 2;
             }
-
             else if (telexCombinations.contains(String.valueOf(input.charAt(i)))) {
-                matchedCombinations.add(String.valueOf(input.charAt(i)));
+                match.add(String.valueOf(input.charAt(i)));
                 i++;
             }
             else {
                 i++;
             }
         }
-        System.out.println( matchedCombinations.size() + " " + matchedCombinations);
+        System.out.println( match.size() + " " + match);
     }
 }
